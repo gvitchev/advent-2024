6c073d382fd10503f7cb9970ae339e308ccdb7b2
gvitchev <gvitchev@gmail.com>
2024-12-05 23:46:19 -0800

    day1

---
+++ day1/day1p1.py
@@ -0,0 +1,25 @@
+SAMPLE = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+
+def solve(input):
+    list1, list2 = [], []
+    for l in input.splitlines():
+        items = l.split()
+        list1.append(int(items[0].strip()))
+        list2.append(int(items[1].strip()))
+    list1.sort()
+    list2.sort()
+    diff = 0
+    for (i, j) in zip(list1, list2):
+        diff += abs(i - j)
+    return diff
+
+if __name__ == "__main__":
+    print(solve(SAMPLE))
+    file = open("input1.txt", "r")
+    content = file.read()
+    print(solve(content))
---
+++ day1/day1p2.py
@@ -0,0 +1,29 @@
+SAMPLE = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+
+def solve(input):
+    list = []
+    list2_counts = {}
+    for l in input.splitlines():
+        items = l.split()
+        list.append(int(items[0].strip()))
+        val2 = int(items[1].strip())
+        if val2 in list2_counts:
+            list2_counts[val2] += 1
+        else:
+            list2_counts[val2] = 1
+    diff = 0
+    for i in list:
+        if i in list2_counts:
+            diff += list2_counts[i] * i
+    return diff
+
+if __name__ == "__main__":
+    print(solve(SAMPLE))
+    file = open("input1.txt", "r")
+    content = file.read()
+    print(solve(content))
